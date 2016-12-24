20e9caa1912654513b66165d9d66247a5d839634
Teng Ma <stmatengss@Tengs-MacBook-Pro.local>
2016-12-25 01:27:04 +0800

    array test

---
+++ cpp11_countainer/array.cpp
@@ -0,0 +1,28 @@
+/*************************************************************************
+    > File Name: array.cpp
+    > Author: stmatengss
+    > Mail: stmatengss@163.com 
+    > Created Time: Sun Dec 25 01:02:32 2016
+ ************************************************************************/
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main() {
+	array<int, 5> myarray = {
+		1,2,3,4,5
+	};
+	for (auto i: myarray) {
+		cout << i <<endl;
+	}
+	for (int i = 0; i < 5; i ++ ) {
+		cout << myarray.at(i) <<endl;
+	}
+	cout << myarray.front() << endl;
+	cout << myarray.back() << endl;
+	array< array<int, 2>, 2> a = {{	{1,2}, {4,5} }};
+	cout << a.max_size() <<endl;
+	cout << endl;
+	return 0;
+}
