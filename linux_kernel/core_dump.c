42231060edbe6dc7f9b4e982398e2eada717272b
stmatengss <805522925@qq.com>
2017-02-13 00:34:06 +0800

    update

---
+++ linux_kernel/core_dump.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: core_dump.c
+    > Author: stmatengss
+    > Mail: stmatengss@163.com 
+    > Created Time: Mon Jan 23 11:48:20 2017
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main() {
+	int *ptr = NULL;
+	*ptr = 10;
+	return 0;
+}
---
+++ test_modual/b.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: ../learning-creating_by_cpp/test_modual/b.cpp
+    > Author: stmatengss
+    > Mail: stmatengss@163.com 
+    > Created Time: Thu Feb  2 11:19:04 2017
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	char *t = nullptr;
+	return 0;
+}
