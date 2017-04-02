d5566ff12b11b6609729b00040697e72bf25bbd7
stmatengss <805522925@qq.com>
2017-04-02 17:54:51 +0800

    add pthread_affinity

--- memcached/main2.c
+++ memcached/main2.c
@@ -16,16 +16,16 @@ int main(int argc, char *argv[])
 	memc = memcached_create(NULL);   
 
 	servers = memcached_server_list_append(NULL, (char*)"localhost", 11211, &rc);   
-	servers = memcached_server_list_append(servers, (char*)"localhost", 30000, &rc);   
+//	servers = memcached_server_list_append(servers, (char*)"localhost", 30000, &rc);   
 
 	rc = memcached_server_push(memc, servers);  
 	memcached_server_free(servers);    
 
-	memcached_behavior_set(memc,MEMCACHED_BEHAVIOR_DISTRIBUTION,MEMCACHED_DISTRIBUTION_CONSISTENT);  
-	memcached_behavior_set(memc, MEMCACHED_BEHAVIOR_RETRY_TIMEOUT, 20) ;  
-	memcached_behavior_set(memc, MEMCACHED_BEHAVIOR_REMOVE_FAILED_SERVERS, 1) ;  // 同时设置MEMCACHED_BEHAVIOR_SERVER_FAILURE_LIMIT 和 MEMCACHED_BEHAVIOR_AUTO_EJECT_HOSTS   
-	memcached_behavior_set(memc, MEMCACHED_BEHAVIOR_SERVER_FAILURE_LIMIT, 5) ;  
-	memcached_behavior_set(memc, MEMCACHED_BEHAVIOR_AUTO_EJECT_HOSTS, 1) ;  
+//	memcached_behavior_set(memc,MEMCACHED_BEHAVIOR_DISTRIBUTION,MEMCACHED_DISTRIBUTION_CONSISTENT);  
+//	memcached_behavior_set(memc, MEMCACHED_BEHAVIOR_RETRY_TIMEOUT, 20) ;  
+//	memcached_behavior_set(memc, MEMCACHED_BEHAVIOR_REMOVE_FAILED_SERVERS, 1) ;  // 同时设置MEMCACHED_BEHAVIOR_SERVER_FAILURE_LIMIT 和 MEMCACHED_BEHAVIOR_AUTO_EJECT_HOSTS   
+//	memcached_behavior_set(memc, MEMCACHED_BEHAVIOR_SERVER_FAILURE_LIMIT, 5) ;  
+//	memcached_behavior_set(memc, MEMCACHED_BEHAVIOR_AUTO_EJECT_HOSTS, 1) ;  
 
 	int time_sl = 0 ;    
 	int times = 0 ;  
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
 
 		//save data    
 		const char  *keys[]= {
-			"key1", "key2", "key3","key4"};   
+			"key1", "key2", "key3","8995"};   
 		const  size_t key_length[]= {
 			4, 4, 4, 4};   
 		char *values[] = {
@@ -47,6 +47,15 @@ int main(int argc, char *argv[])
 
 			rc = memcached_set(memc, keys[i], key_length[i], values[i], val_length[i], (time_t)180,(uint32_t)0);     printf("key: %s  rc:%s\n", keys[i], memcached_strerror(memc, rc));   // 输出状态         
 		}   
+		i = 0;
+		size_t* value_size;
+		uint32_t* flag;
+		memcached_return_t* error;
+		for (; i < 4; i++)
+		{
+			char* res = memcached_get(memc, keys[i], key_length[i], value_size, flag, error);
+			printf("get:%s\n", res);
+		}
 		printf("time: %d\n", time_sl++) ;  
 		sleep(1) ;  
 
