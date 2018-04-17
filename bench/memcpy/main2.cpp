af9e0ddb821295fd229373adfe37e0bc56e4d795
stmatengss <805522925@qq.com>
2018-04-17 13:03:26 +0800

    add memset/fill bench

--- bench/memcpy/main2.cpp
+++ bench/memcpy/main2.cpp
@@ -15,13 +15,12 @@ uint64_t co = 0xdeadbeaf;
 int main() {
 	char *mem = new char[iter_num * length];
 	auto start = chrono::system_clock::now();
-	for (int i = 0; i < iter_num; i ++ ) {
-		co += 1;
-		memcpy(mem + i * length, &co, length);
-//		*(uint64_t *)(mem + i * length) = co;
-//		fill(mem, mem + length, 0);
-//		printf("num=%d\n", a);
-	}
+	/* BENCH MEMCPY */
+	memset(mem, 0, iter_num * length);
+
+	/* BENCH FILL */
+//	fill(mem, mem + iter_num * length, 0);
+
 	auto end = chrono::system_clock::now();
 	std::chrono::duration<double> diff = end-start;
 	printf("[TIME]%lf\n", diff.count());
