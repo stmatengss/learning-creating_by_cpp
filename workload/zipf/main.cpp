ce5fc576d5734dc3278513115add9d92b59e898a
stmatengss <805522925@qq.com>
2018-07-12 15:54:41 +0800

    add zipf dist test script

--- workload/zipf/main.cpp
+++ workload/zipf/main.cpp
@@ -1,79 +1,30 @@
-#include <random>
 #include <iostream>
-#include <cmath>
-#include <algorithm>
 
+#include "zipf_gen.hpp"
 
 using namespace std;
 
-using ll = long long;
-
-class ZipfGen
-{
-public:
-    random_device rd;
-    double alpha;
-    ll item_max;
-    double *hist;
-    double *dist;
-    double *hist_value;
-    ll *value;
-
-    ZipfGen(double alpha = 0.99, ll item_max = 1000): alpha(alpha), item_max(item_max) {
-        hist = new double[item_max + 1];
-        hist_value = new double[item_max + 1];
-        dist = new double[item_max];
-        value = new ll[item_max];
-        cout << "Alpha=" << alpha << endl;
-        gen_distribution();
-        gen_value();
-    }
-    ~ZipfGen() = default;
-    
-    void gen_distribution() {
-        hist[0] = 0.0;
-        hist_value[0] = 0.0;
-        double dist_sum = 0.0;
-        for (int i = 1; i <= item_max; i ++ ) {
-            dist_sum += (1.0 / pow((double)i, alpha));
-            // cout << dist_sum << endl;
-        }
-
-        for (int i = 1; i <= item_max; i ++ ) {
-            double pro = (1.0 / pow((double)i, alpha)) / dist_sum;
-            hist[i] = hist[i - 1] + pro;
-            hist_value[i] = hist[i] * item_max;
-            dist[i - 1] = pro;
-            // cout << hist[i] << endl;
-        }
-
-    }
-
-    void gen_value() {
-        mt19937 g(rd());
-        for (int i = 1; i <= item_max; i ++ ) {
-            value[i - 1] = i;
-        }
-        shuffle(value, value + item_max, g);
-    }
-
-    ll next_long() {
-        double rd_num = rd() % item_max;
-        int index = (upper_bound(hist_value, hist_value + item_max, rd_num) - hist_value);
-        return value[index - 1];
-    }
-
-    vector<ll> get_dataset(int length) {
-        // TODO
-        return vector<ll>();
-    }
-
-};
-
-int main() {
+void test() {
     ZipfGen *zipf = new ZipfGen();
     for (int i = 0; i < 100000; i ++ ) {
         cout << zipf->next_long() << endl;
     }
+}
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 4) {
+        cout << "usage: ./main alpha " << endl;
+        exit(-1);
+    }
+    double alpha = stod(argv[1]);
+    double item = stoll(argv[2]);
+    double len = stoll(argv[3]);
+
+    ZipfGen *zipf = new ZipfGen(alpha, item);
+    for (int i = 0; i < len; i ++ ) {
+        cout << zipf->next_long() << endl;
+    }
+
     return 0;
 }
--- workload/zipf/test.sh
+++ workload/zipf/test.sh
@@ -1,1 +1,1 @@
-./main | sort -n | uniq -c | sort -n -t 1
+./main $1 $2 $3 | sort -n | uniq -c | sort -n -t 1
