5c6163db205118fe85b163b9a2b1ebeda66f4e87
stmatengss <805522925@qq.com>
2020-05-17 11:50:39 +0800

    add new c++17 features

---
+++ cpp11/inherit/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+class Base {
+public:
+    int value1;
+    int value2;
+    Base() {
+        value1 = 1;
+    }
+    Base(int value) : Base() { // 委托 Base() 构造函数
+        value2 = value;
+    }
+};
+class Subclass : public Base {
+public:
+    using Base::Base; // 继承构造
+};
+int main() {
+    Subclass s(3);
+    std::cout << s.value1 << std::endl;
+    std::cout << s.value2 << std::endl;
+}
---
+++ cpp11/source_location/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string_view>
+#include <source_location>
+ 
+void log(std::string_view message,
+         const std::source_location& location = std::source_location::current())
+{
+    std::cout << "info:"
+              << location.file_name() << ":"
+              << location.line() << " "
+              << message << '\n';
+}
+ 
+int main()
+{
+    log("Hello world!");
+}
